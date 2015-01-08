43022ea17586c4744a8919f3b52b72a526da73da
Mark <mmccask2@gmu.edu>
2015-01-07 23:35:16 -0500

    Add main outline

--- main.cpp
+++ main.cpp
@@ -1,3 +1,28 @@
 #include <cstdlib>
 #include <iostream>
 
+/*
+ * Add command line arguments
+ * ideas:
+ * difficulty (speed), color, graphics 
+ * debugging, etc.
+ */
+
+
+int main( void )
+{
+  //load board
+  //set up screen
+  while( true ) //game loop
+    {
+      //take user input
+      //action from input
+      //check for collisions
+      //check for lines? (maybe should go somewhere else)
+      //update board
+      //update screen
+      if( true ) //end game conditional
+	break;
+    }
+  exit( EXIT_SUCCESS );
+}
